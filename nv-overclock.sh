1e40c20a440597d4f59538a8e4f29867558a4aa0
avtrev <trevorhweir@gmail.com>
2021-06-23 14:09:14 -0400

    added -i interface and -p power options -made -i option manditory for most functionality

--- nv-overclock.sh
+++ nv-overclock.sh
@@ -6,10 +6,7 @@
 #sudo nvidia-xconfig --cool-bits=31 --allow-empty-initial-configuration
 #reboot
 
-coreMin=$(sudo $nv nvidia-settings -q [gpu:0]/GPUGraphicsClockOffset[3] | grep -i range | awk '{print $10}')
-coreMax=$(sudo $nv nvidia-settings -q [gpu:0]/GPUGraphicsClockOffset[3] | grep -i range | awk '{print $12}')
-memMin=$(sudo $nv nvidia-settings -q [gpu:0]/GPUMemoryTransferRateOffset[3] | grep -i range | awk '{print $10}')
-memMax=$(sudo $nv nvidia-settings -q [gpu:0]/GPUMemoryTransferRateOffset[3] | grep -i range | awk '{print $12}')
+
 
 #enable persistant mode
 enable_pm () {
@@ -21,6 +18,8 @@ enable_pm () {
 help () {
     echo "overclock.sh help"
     echo -e "-h\t\t\tdisplay help menu"
+    echo -e "-i\t\t\tset interface device ID"
+    echo -e "-p\t\t\tset power level in max watts"
     echo -e "-q\t\t\tquery current fan speed, core clock, memory clock"
     echo -e "-d\t\t\trestore default fan speed and clock frequencies"
     echo -e "-f <fan_speed>\t\tchange gpu fan speed between 0 - 100"
@@ -28,7 +27,11 @@ help () {
     echo -e "-m <memory_offset>\tchange memory clock offset between $memMin - $memMax"
 }
 
-
+selectDevice () {
+    echo '-i option must be present to select a device ID'
+    echo 'select from avaible gpus'
+    nvidia-smi --list-gpus
+}
 
 #if nv does not exist - append export nv to .bashrc
 if ! [[ $nv ]];
@@ -39,65 +42,123 @@ if ! [[ $nv ]];
 fi
 
 
-while getopts :dqhf:c:m: option; do
+
+
+while getopts :dqhi:p:f:c:m: option; do
     case $option in
+        #Set Interface Device
+        i) deviceID=$OPTARG
+            echo 'option i present'
+            echo "device ID = $deviceID"
+            sudo $nv nvidia-settings -a [gpu:$deviceID]/GpuPowerMizerMode=1 #set powermizer mode to prefer max performance
+            coreMin=$(sudo $nv nvidia-settings -q [gpu:$deviceID]/GPUGraphicsClockOffsetAllPerformanceLevels | grep -i range | awk '{print $10}')
+            coreMax=$(sudo $nv nvidia-settings -q [gpu:$deviceID]/GPUGraphicsClockOffsetAllPerformanceLevels | grep -i range | awk '{print $12}')
+            memMin=$(sudo $nv nvidia-settings -q [gpu:$deviceID]/GPUMemoryTransferRateOffsetAllPerformanceLevels | grep -i range | awk '{print $10}')
+            memMax=$(sudo $nv nvidia-settings -q [gpu:$deviceID]/GPUMemoryTransferRateOffsetAllPerformanceLevels | grep -i range | awk '{print $12}')
+            fan1ID=$(($deviceID * 2))
+            fan2ID=$(($fan1 + 1))
+        ;;
         #Restore Defaults
         d)
-            echo 'option d present'
-            sudo $nv nvidia-settings -a [gpu:0]/GPUFanControlState=0;
-            sudo $nv nvidia-settings -a [gpu:0]/GPUGraphicsClockOffset[3]=0
-            sudo $nv nvidia-settings -a [gpu:0]/GPUMemoryTransferRateOffset[3]=0
-            sudo nvidia-smi -pm 0
-            echo 'restored defaults'
-            exit 0
+            if [ $deviceID ];
+            then
+                echo 'option d present'
+                sudo $nv nvidia-settings -a [gpu:$deviceID]/GPUFanControlState=0; #restore fan control state to default
+                sudo $nv nvidia-settings -a [gpu:$deviceID]/GPUGraphicsClockOffsetAllPerformanceLevels=0 #restore core clock offset to default
+                sudo $nv nvidia-settings -a [gpu:$deviceID]/GPUMemoryTransferRateOffsetAllPerformanceLevels=0 #restore memory clock offset to default
+                sudo nvidia-smi -pm 0
+                echo 'restored defaults'
+                exit 0
+            else
+                selectDevice
+                exit 0
+            fi
         ;;
         #Query Speeds
         q)
-            echo 'option q present'
-            sudo $nv nvidia-settings -q [fan:0]/GPUTargetFanSpeed
-            sudo $nv nvidia-settings -q [gpu:0]/GPUGraphicsClockOffset[3]
-            sudo $nv nvidia-settings -q [gpu:0]/GPUMemoryTransferRateOffset[3]
-            exit 0
+            if [ $deviceID ];
+            then
+                echo 'option q present'
+                sudo $nv nvidia-settings -q [gpu:$deviceID]/NvidiaDriverVersion
+                sudo $nv nvidia-settings -q [fan:$fan1ID]/GPUTargetFanSpeed #query fan1 speed
+                sudo $nv nvidia-settings -q [fan:$fan2ID]/GPUTargetFanSpeed #query fan2 speed
+                sudo $nv nvidia-settings -q [gpu:$deviceID]/GPUGraphicsClockOffsetAllPerformanceLevels #query core speed
+                sudo $nv nvidia-settings -q [gpu:$deviceID]/GPUMemoryTransferRateOffsetAllPerformanceLevels #query memory speed
+                sudo $nv nvidia-settings -q [gpu:$deviceID]/GPUCurrentClockFreqs #query all clock speeds
+                exit 0
+            else
+                selectDevice
+                exit 0
+            fi
+        ;;
+        #Power Level
+        p) power=$OPTARG
+            if [ $deviceID ];
+            then
+                sudo nvidia-smi -i $deviceID -pl $power
+            else
+                selectDevice
+                exit 0
+            fi
         ;;
         #Fan Speed
         f) fan=$OPTARG
-            echo 'option f present'
-            if [ $fan -ge 0 ] && [ $fan -le 100 ];
-                then
-                    echo "set gpu fan $fan%";
-                    sudo $nv nvidia-settings -a [gpu:0]/GPUFanControlState=1;
-                    sudo $nv nvidia-settings -a [fan:0]/GPUTargetFanSpeed=$fan;
+            if [ $deviceID ];
+            then
+                echo 'option f present'
+                if [ $fan -ge 0 ] && [ $fan -le 100 ];
+                    then
+                        echo "set gpu fan $fan%";
+                        sudo $nv nvidia-settings -a [gpu:$deviceID]/GPUFanControlState=1; #enable fan control
+                        sudo $nv nvidia-settings -a [fan:$fan1ID]/GPUTargetFanSpeed=$fan; #set fan1 speed
+                        sudo $nv nvidia-settings -a [fan:$fan2ID]/GPUTargetFanSpeed=$fan; #set fan2 speed
 
-                else
-                    echo 'value must be between 0 - 100'
-                    exit 0
+                    else
+                        echo 'value must be between 0 - 100'
+                        exit 0
+                fi
+            else
+                selectDevice
+                exit 0
             fi
         ;;
         #Core Clock Offset
         c) core=$OPTARG
-            echo 'option c present'
-            if [ $core -ge $coreMin ] && [ $core -le $coreMax ];
+            if [ $deviceID ];
             then
-                enable_pm
-                echo "set gpu core frequency offset $core"
-                sudo $nv nvidia-settings -a [gpu:0]/GPUGraphicsClockOffset[3]=$core
+                echo 'option c present'
+                if [ $core -ge $coreMin ] && [ $core -le $coreMax ];
+                then
+                    enable_pm
+                    echo "set gpu core frequency offset $core"
+                    sudo $nv nvidia-settings -a [gpu:$device]/GPUGraphicsClockOffsetAllPerformanceLevels=$core #set core frequency
+                else
+                    echo "core frequency offset must be between $coreMin - $coreMax"
+                    exit 0
+                fi
             else
-                echo "core frequency offset must be between $coreMin - $coreMax"
+                selectDevice
                 exit 0
             fi
         ;;
         #Memory Clock Offset
         m) memory=$OPTARG
-            echo 'option m present'
-            if [ $memory -ge $memMin ] && [ $memory -le $memMax ];
+            if [ $deviceID ];
             then
-                enable_pm
-                echo "set gpu memory frequency offset $memory"
-                sudo $nv nvidia-settings -a [gpu:0]/GPUMemoryTransferRateOffset[3]=$memory
+                echo 'option m present'
+                if [ $memory -ge $memMin ] && [ $memory -le $memMax ];
+                then
+                    enable_pm
+                    echo "set gpu memory frequency offset $memory"
+                    sudo $nv nvidia-settings -a [gpu:$device]/GPUMemoryTransferRateOffsetAllPerformanceLevels=$memory #set memory frequency
+                else
+                    echo "memory frequency offest must be between $memMin - $memMax"
+                fi
+                echo $memory
             else
-                echo "memory frequency offest must be between $memMin - $memMax"
+                selectDevice
+                exit 0
             fi
-            echo $memory
         ;;
         #Display Help
         h | *)
