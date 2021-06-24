6fb07e4115172f00369e8d33f755547c3895de2a
avtrev <trevorhweir@gmail.com>
2021-06-24 11:31:03 -0400

    pre push to remote commit

--- nv-overclock.sh
+++ nv-overclock.sh
@@ -1,47 +1,63 @@
 #!/bin/bash
 
+#USE AT YOUR OWN RISK!!
+#
+# this script is set up for dual fan nvidia gpus only at the moment
+# i can't find a command that will give me a fan count per gpu
+# one option is to change a fan speeds for all gpus at the same time
 
-#in order to change clock offsets, coolbits must be enabled
-#run this command in terminal
+# in order to change clock offsets, coolbits must be enabled
+# run this command in terminal
+#
 #sudo nvidia-xconfig --cool-bits=31 --allow-empty-initial-configuration
 #reboot
 
+# this is what i use on my system to keep things simple
+# if nv does not exist then append export nv to .bashrc
+#
+# if ! [[ $nv ]];
+#     then
+#         xAuthPath=$(ps a |grep X | awk '{print $10}')
+#         echo -e "\n#nv xrdp / ssh display auth path " | cat >> ~/.bashrc
+#         echo -e "export nv=\"DISPLAY=:0 XAUTHORITY=$xAuthPath\"" | cat >> ~/.bashrc
+# fi
 
+#variable for display authority
+xAuthPath=$(ps a |grep X | awk '{print $10}')
+nv="DISPLAY=:0 XAUTHORITY=$xAuthPath"
 
 #enable persistant mode
 enable_pm () {
     echo 'enable persistant mode'
-    sudo nvidia-smi -pm 1  
+    sudo nvidia-smi -pm 1
+}
+
+#set pomermizer mode
+setPowerMizerMode1() {
+    sudo $nv nvidia-settings -a [gpu:$deviceID]/GpuPowerMizerMode=1 #set powermizer mode to prefer max performance
 }
 
 #display help menu
 help () {
-    echo "overclock.sh help"
+    echo "nv-overclock.sh help"
     echo -e "-h\t\t\tdisplay help menu"
-    echo -e "-i\t\t\tset interface device ID"
-    echo -e "-p\t\t\tset power level in max watts"
     echo -e "-q\t\t\tquery current fan speed, core clock, memory clock"
     echo -e "-d\t\t\trestore default fan speed and clock frequencies"
+    echo -e "-i <device_id>\t\tset interface device ID"
+    echo -e "-p <max_watts>\t\tset power level in max watts"
     echo -e "-f <fan_speed>\t\tchange gpu fan speed between 0 - 100"
     echo -e "-c <core_offset>\tchange core clock offset between $coreMin - $coreMax"
     echo -e "-m <memory_offset>\tchange memory clock offset between $memMin - $memMax"
+    echo
+    selectDeviceMessage
 }
 
-selectDevice () {
+
+selectDeviceMessage () {
     echo '-i option must be present to select a device ID'
-    echo 'select from avaible gpus'
+    echo 'select from available gpus:'
     nvidia-smi --list-gpus
 }
-
-#if nv does not exist - append export nv to .bashrc
-if ! [[ $nv ]];
-    then
-        xAuthPath=$(ps a |grep X | awk '{print $10}')
-        echo -e "\n#nv xrdp / ssh display auth path " | cat >> ~/.bashrc
-        echo -e "export nv=\"DISPLAY=:0 XAUTHORITY=$xAuthPath\"" | cat >> ~/.bashrc
-fi
-
-
 
 
 while getopts :dqhi:p:f:c:m: option; do
@@ -50,7 +66,6 @@ while getopts :dqhi:p:f:c:m: option; do
         i) deviceID=$OPTARG
             echo 'option i present'
             echo "device ID = $deviceID"
-            sudo $nv nvidia-settings -a [gpu:$deviceID]/GpuPowerMizerMode=1 #set powermizer mode to prefer max performance
             coreMin=$(sudo $nv nvidia-settings -q [gpu:$deviceID]/GPUGraphicsClockOffsetAllPerformanceLevels | grep -i range | awk '{print $10}')
             coreMax=$(sudo $nv nvidia-settings -q [gpu:$deviceID]/GPUGraphicsClockOffsetAllPerformanceLevels | grep -i range | awk '{print $12}')
             memMin=$(sudo $nv nvidia-settings -q [gpu:$deviceID]/GPUMemoryTransferRateOffsetAllPerformanceLevels | grep -i range | awk '{print $10}')
@@ -70,7 +85,7 @@ while getopts :dqhi:p:f:c:m: option; do
                 echo 'restored defaults'
                 exit 0
             else
-                selectDevice
+                selectDeviceMessage
                 exit 0
             fi
         ;;
@@ -79,7 +94,7 @@ while getopts :dqhi:p:f:c:m: option; do
             if [ $deviceID ];
             then
                 echo 'option q present'
-                sudo $nv nvidia-settings -q [gpu:$deviceID]/NvidiaDriverVersion
+                sudo $nv nvidia-settings -q [gpu:$deviceID]/NvidiaDriverVersion #query nvidia driver version
                 sudo $nv nvidia-settings -q [fan:$fan1ID]/GPUTargetFanSpeed #query fan1 speed
                 sudo $nv nvidia-settings -q [fan:$fan2ID]/GPUTargetFanSpeed #query fan2 speed
                 sudo $nv nvidia-settings -q [gpu:$deviceID]/GPUGraphicsClockOffsetAllPerformanceLevels #query core speed
@@ -87,7 +102,7 @@ while getopts :dqhi:p:f:c:m: option; do
                 sudo $nv nvidia-settings -q [gpu:$deviceID]/GPUCurrentClockFreqs #query all clock speeds
                 exit 0
             else
-                selectDevice
+                selectDeviceMessage
                 exit 0
             fi
         ;;
@@ -95,9 +110,15 @@ while getopts :dqhi:p:f:c:m: option; do
         p) power=$OPTARG
             if [ $deviceID ];
             then
-                sudo nvidia-smi -i $deviceID -pl $power
+                if [ $power ];
+                then
+                    sudo nvidia-smi -i $deviceID -pl $power
+                else
+                    echo "must enter power in watts"
+                    exit 0
+                fi
             else
-                selectDevice
+                selectDeviceMessage
                 exit 0
             fi
         ;;
@@ -118,7 +139,7 @@ while getopts :dqhi:p:f:c:m: option; do
                         exit 0
                 fi
             else
-                selectDevice
+                selectDeviceMessage
                 exit 0
             fi
         ;;
@@ -130,14 +151,15 @@ while getopts :dqhi:p:f:c:m: option; do
                 if [ $core -ge $coreMin ] && [ $core -le $coreMax ];
                 then
                     enable_pm
+                    setPowerMizerMode1
                     echo "set gpu core frequency offset $core"
-                    sudo $nv nvidia-settings -a [gpu:$device]/GPUGraphicsClockOffsetAllPerformanceLevels=$core #set core frequency
+                    sudo $nv nvidia-settings -a [gpu:$deviceID]/GPUGraphicsClockOffsetAllPerformanceLevels=$core #set core frequency
                 else
                     echo "core frequency offset must be between $coreMin - $coreMax"
                     exit 0
                 fi
             else
-                selectDevice
+                selectDeviceMessage
                 exit 0
             fi
         ;;
@@ -149,14 +171,15 @@ while getopts :dqhi:p:f:c:m: option; do
                 if [ $memory -ge $memMin ] && [ $memory -le $memMax ];
                 then
                     enable_pm
+                    setPowerMizerMode1
                     echo "set gpu memory frequency offset $memory"
-                    sudo $nv nvidia-settings -a [gpu:$device]/GPUMemoryTransferRateOffsetAllPerformanceLevels=$memory #set memory frequency
+                    sudo $nv nvidia-settings -a [gpu:$deviceID]/GPUMemoryTransferRateOffsetAllPerformanceLevels=$memory #set memory frequency
                 else
                     echo "memory frequency offest must be between $memMin - $memMax"
                 fi
                 echo $memory
             else
-                selectDevice
+                selectDeviceMessage
                 exit 0
             fi
         ;;
